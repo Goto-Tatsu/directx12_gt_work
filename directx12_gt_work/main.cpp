9aebd10bae70f2d16a55c6dadc7ece15e2bd69e3
Goto-Tatsu <50894018+Goto-Tatsu@users.noreply.github.com>
2023-10-06 22:53:51 +0900

    feat： :sparkles: [add] "main.cpp"  コマンドベースで起動することを確認する

---
+++ directx12_gt_work/main.cpp
@@ -0,0 +1,33 @@
+#include <Windows.h>
+#ifdef _DEBUG
+#include <iostream>
+#endif // !_DEBUG
+
+using namespace std;
+
+// @brief コンソール画面にフォーマット付き文字列を表示
+// @param formatフォーマット（%dとか%fとかの）
+// @param 可変長引数
+// @remarks この関数はデバッグ用です。デバッグ時にしか動作しません。
+
+void DebugOutputFormatString(const char* format, ...)
+{
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	printf(format, valist);
+	va_end(valist);
+#endif // !_DEBUG
+}
+
+#ifdef _DEBUG
+int main()
+{
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+#endif // _DEBUG
+	DebugOutputFormatString("Show window test.");
+	getchar();
+	return 0;
+}
