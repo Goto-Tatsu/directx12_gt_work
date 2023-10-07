672ba5d913d4d8433e739c031a93163bad33106c
Goto-Tatsu <50894018+Goto-Tatsu@users.noreply.github.com>
2023-10-07 14:46:23 +0900

    feat：#2 ウィンドウを生成する

--- directx12_gt_work/main.cpp
+++ directx12_gt_work/main.cpp
@@ -1,7 +1,11 @@
 #include <Windows.h>
+#include <tchar.h>
 #ifdef _DEBUG
 #include <iostream>
 #endif // !_DEBUG
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
 
 using namespace std;
 
@@ -9,6 +13,8 @@ using namespace std;
 // @param formatフォーマット（%dとか%fとかの）
 // @param 可変長引数
 // @remarks この関数はデバッグ用です。デバッグ時にしか動作しません。
+
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 void DebugOutputFormatString(const char* format, ...)
 {
@@ -23,11 +29,74 @@ void DebugOutputFormatString(const char* format, ...)
 #ifdef _DEBUG
 int main()
 {
+	// ウィンドウクラスの生成＆登録
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProc;		// コールバック関数の指定
+	w.lpszClassName = _T("DX12Study");							// アプリケーションンクラス名（適当でよい）
+	w.hInstance = GetModuleHandle(nullptr);				// ハンドルの取得
+
+	RegisterClassEx(&w);		// アプリケーションクラス（ウィンドウクラスの指定をOSに伝える）
+
+	RECT wrc = { (LONG)0, (LONG)0, (LONG)WINDOW_WIDTH, (LONG)WINDOW_HEIGHT };	// ウィンドウサイズを決める
+	// 関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	// ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,	// クラス名指定
+		_T("DX12Study"),							// タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,	// タイトルバーと境界線があるウィンドウ
+		CW_USEDEFAULT,					// 表示X座標はOSにお任せ
+		CW_USEDEFAULT,					// 表示Y座標はOSにお任せ
+		wrc.right - wrc.left,					// ウィンドウ幅
+		wrc.bottom - wrc.top,				// ウィンドウ高
+		nullptr,									// 親ウィンドウハンドル
+		nullptr,									// メニューハンドル
+		w.hInstance,							// 呼び出しアプリケーションハンドル
+		nullptr);									// 追加パラメーター
+
+	// ウィンドウ表示
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = {};
+
+	while (true)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+
+	// もうクラスは使わないので登録解除する
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
 #else
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-#endif // _DEBUG
+#endif // !_DEBUG
 	DebugOutputFormatString("Show window test.");
 	getchar();
 	return 0;
+}
+
+//==============================
+// ウィンドウの生成
+//==============================
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	// ウィンドウが破棄されたら呼ばれる
+	if(msg == WM_DESTROY)
+	{
+		PostQuitMessage(0);		// OSに対して「もうこのアプリは終わる」と伝える
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);	// 既定の処理を行う
 }
